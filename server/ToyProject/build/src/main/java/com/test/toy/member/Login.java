66567d185244907f8b18a0eec582e06b19837bc7
JIYEON <104612045+GitOfJY@users.noreply.github.com>
2022-06-29 00:04:24 +0900

    Delete server/ToyProject/build directory

--- server/ToyProject/build/src/main/java/com/test/toy/member/Login.java
+++
@@ -1,8 +0,0 @@
-package com.test.toy.member;
-
-import javax.servlet.annotation.WebServlet;
-
-@WebServlet("/member/login.do")
-public class Login extends {
-
-}
--- server/ToyProject/build/src/main/java/com/test/toy/member/Unregister.java
+++
@@ -1,6 +0,0 @@
-package com.test.toy.member;
-
-public class Unregister extends HttpServlet {
-	
-	
-}
